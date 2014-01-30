d9e7ada03e9e3b04007838411759d0727a16677a
Marvin Oßwald <marvinosswald@marvins-mbp.fritz.box>
2014-01-30 20:52:23 +0100

    Initial Commit

---
+++ pyLoad OSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pyLoad OSX
+//
+//  Created by Marvin Oßwald on 30.01.14.
+//  Copyright (c) 2014 Marvin Oßwald. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ pyLoad OSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  pyLoad OSX
+//
+//  Created by Marvin Oßwald on 30.01.14.
+//  Copyright (c) 2014 Marvin Oßwald. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
